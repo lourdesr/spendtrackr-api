864fb6e87ec84d1e456a8e7a128a78e05b39db41
Lourdes Roashan <root.roashan@gmail.com>
2024-08-27 21:56:44 -0400

    wip

--- migrations/20240823161042_stores.js
+++ migrations/20240823161042_stores.js
@@ -6,7 +6,8 @@ exports.up = (knex) => knex.schema.createTable('stores', (table) => {
   table.increments('id');
   table.string('slug').notNullable().unique();
   table.string('name').notNullable();
-  table.string('location').notNullable();
+  table.string('state');
+  table.string('city').notNullable();
   table.string('country').notNullable();
   table.timestamps(true, true);
 });
